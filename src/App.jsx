4af34e67f25decc56b18805c0ebce2cb9d2e2e26
Adarsh <adarshtrip2306@gmail.com>
2025-02-21 17:41:17 +0530

    updated the folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,6 @@
-import React from 'react'
-
+import React from "react";
 const App = () => {
-  return (
-    <div>
-      <h1 className='bg-red-70'>Hello</h1>
-    </div>
-  )
-}
+	return;
+};
 
-export default App
+export default App;
